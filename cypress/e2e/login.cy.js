17bb45eb399149b933c2e3c71e9c15ea3fda012a
PedroMendes414 <pedrooliverio4@gmail.com>
2025-10-13 21:04:07 -0300

    adicionando hook beforeEach para o acesso a pagina de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,9 @@
 describe('Login', ()=> {
-    it('Login com dados válidos deve permitir entrada no sistema', ()=> {
+    beforeEach(()=> {
         cy.visit('http://localhost:4000')
+    })
+
+    it('Login com dados válidos deve permitir entrada no sistema', ()=> {
         cy.get('#username').click().type('julio.lima')
         cy.get('#senha').click().type('123456')
         cy.contains('button', 'Entrar').click()
@@ -9,11 +12,10 @@ describe('Login', ()=> {
     })
 
     it('Login com inválidos válidos deve apresentar mensagem de erro', ()=> {
-        cy.visit('http://localhost:4000')
         cy.get('#username').click().type('julio.lima')
         cy.get('#senha').click().type('54321')
         cy.contains('button', 'Entrar').click()
 
         cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
     })
-})  
+}) 
